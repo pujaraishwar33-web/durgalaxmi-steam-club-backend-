cd64dd2fc6d86db31030adefbf1b32c197adbf68
pujaraishwar33-web <pujaraishwar33@gmail.com>
2026-09-07 14:23:45 +0545

    Use HTTPS for Cloudinary images

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -1,25 +1,20 @@
 from rest_framework import serializers
-
 from .models import Gallery
 
 
 class GallerySerializer(serializers.ModelSerializer):
-
     image = serializers.SerializerMethodField()
 
     class Meta:
-
         model = Gallery
-
         fields = "__all__"
 
     def get_image(self, obj):
-
         if not obj.image:
             return None
 
         try:
-            return obj.image.url
-
+            url = obj.image.url
+            return url.replace("http://", "https://")
         except Exception:
             return None
